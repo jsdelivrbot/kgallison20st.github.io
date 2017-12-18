bdd2b5bd97487e2a1f952bb83bafad6efaefb98c
Kieran Gallison <kgallison20st@bishopstang.org>
2017-12-18 14:59:51 +0000

    Offset circles in geometric_pattern

--- Seizure_Warning/main.js
+++ Seizure_Warning/main.js
@@ -1,22 +1,37 @@
 var NUM_CIRCLES = 12;
 var circleDiameter;
+var circleRadius;
 
 function setup() {
     createCanvas(480, 600);
     circleDiameter = width/NUM_CIRCLES;
+    circleRadius = circleDiameter/2;
 }
 
 function draw() {
+    var isShifted = false;
+    
     var y = 0;
-    while (y <= height) {
-        var x = 0;
+    while (y <= height) { 
+        
+        var x;
+        
+        if (isShifted) {
+            x = circleRadius;
+        } else {
+            x = 0;
+        }
+        
         while (x <= width) {
             ellipse(x, y, circleDiameter, circleDiameter);
             x = x + circleDiameter;
     }
     
-    y = y + circleDiameter;
+    y = y + circleRadius;
+    isShifted = !isShifted; 
   }
+}
+  
     //ellipse(0, height/2, circleDiameter, circleDiameter);
     //ellipse(circleDiameter, height/2, circleDiameter, circleDiameter);
     //ellipse(2*circleDiameter, height/2, circleDiameter, circleDiameter);
@@ -26,4 +41,3 @@ function draw() {
     //ellipse(6*circleDiameter, height/2, circleDiameter, circleDiameter);
     //ellipse(7*circleDiameter, height/2, circleDiameter, circleDiameter);
     //ellipse(8*circleDiameter, height/2, circleDiameter, circleDiameter);
-}
